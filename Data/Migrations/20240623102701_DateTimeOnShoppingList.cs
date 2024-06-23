4e2eb8e2f720e483842a744628610a453d0528f3
Paweł Dahlke <dahlkepawelcontact@gmail.com>
2024-06-23 12:28:51 +0200

    Generacja Migracji DateTime w shoppingList

---
+++ Data/Migrations/20240623102701_DateTimeOnShoppingList.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace lab1.net.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class DateTimeOnShoppingList : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Planned",
+                table: "ShoppingList",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Planned",
+                table: "ShoppingList");
+        }
+    }
+}
